7cbc8d8a447a56f5234eda90cc48ab5cce5d088e
AukiJuanDiaz <hauke.diers@outlook.com>
2018-05-14 11:06:37 +0200

    Delete the too northern trips from database + create new condensed trips of the reminaing set

--- KielGdynia/SQL/DeleteoutOfBounds.sql
+++ KielGdynia/SQL/DeleteoutOfBounds.sql
@@ -2,6 +2,6 @@ DELETE FROM kiel_import_complete
 WHERE TripID IN (SELECT p.TripID
 					  FROM (SELECT *
 					  		  FROM kiel_import_complete
-					  		  WHERE (Longitude <= 9.86) || (Longitude >= 20.0) || (Latitude >= 56.26)
+					  		  WHERE (Longitude <= 9.86) || (Longitude >= 20.0) || (Latitude >= 55.45)
 					  		  GROUP BY TripID) as p)
 					  
