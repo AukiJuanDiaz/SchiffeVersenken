0a2ae7775b4a4c84c880044d417edfb59a39f48e
AukiJuanDiaz <hauke.diers@outlook.com>
2018-06-18 12:50:40 +0200

    Add a default map in the MapView of GUI

--- java/src/sd/group03/MapView.java
+++ java/src/sd/group03/MapView.java
@@ -1,5 +1,31 @@
 package sd.group03;
 
-public class MapView {
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+public class MapView extends JPanel{
+
+    private BufferedImage image;
+
+    public MapView() {
+       try {                
+          image = ImageIO.read(new File("H:\\git\\group03\\java\\src\\sd\\group03\\BackgroundDefaultMap.png"));
+       } catch (IOException ex) {
+            System.out.println("error opeining the background image");
+       }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(image, 0, 0, this); // see javadoc for more info on the parameters            
+    }
 
 }
+
