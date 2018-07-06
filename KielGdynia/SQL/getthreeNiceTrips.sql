969a655010adde9b5d469f37474b58ca0908a027
AukiJuanDiaz <hauke.diers@outlook.com>
2018-07-06 18:29:53 +0200

    Add modified SQL for Selection of three Kiel Trips

--- KielGdynia/SQL/getthreeNiceTrips.sql
+++ KielGdynia/SQL/getthreeNiceTrips.sql
@@ -14,7 +14,8 @@ SELECT 	dp.AgentLatitude,
 			dp.SOG, 
 			dp.COG,
 			dp.TripID,
-			dp.TimeDate
+			CONCAT("'",dp.TimeDate,"'"),
+			RemainingTravelTimeInMinutes
 FROM kiel_import_complete AS dp
 WHERE TripID IN (183783, 1992358, 393533)
 
